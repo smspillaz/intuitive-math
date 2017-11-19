fee9afa1cabcc56eb169c7eb2f847dc06955638d
Sam Spilsbury <smspillaz@gmail.com>
2017-11-19 14:19:03 +0800

    server: Add support for serverless
    You will need to manually set the binary content types on the API
    Gateway itself. Otherwise you can just use npm run serverless-deploy --
    --stage staging to deploy straight to a stage

---
+++ lambda.js
@@ -0,0 +1,12 @@
+const serverless = require('serverless-http');
+const app = require('./server/server');
+
+exports.handler = serverless(app, {
+  binary: [
+    'font/eot',
+    'font/opentype',
+    'font/otf',
+    'image/jpeg',
+    'image/png',
+  ],
+});
