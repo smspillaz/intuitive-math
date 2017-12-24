adf7a49b4b775423d22d5c1df2d3ba24be369836
Sam Spilsbury <smspillaz@gmail.com>
2017-12-24 18:04:39 +0800

    pages: Add some more on plane-based linear independence

--- app/containers/LinearAlgebraPage/index.js
+++ app/containers/LinearAlgebraPage/index.js
@@ -853,6 +853,70 @@ const LinearIndependenceSection = () => (
       and <MathJax.Node inline>{'(1, 2)'}</MathJax.Node>.
     </p>
     <MathJax.Node inline>{'1 * ((1, 2) - (1, 1)) + 4 * (1, 1) = 1 * (0, 1) + 4 * (1, 1) = (4, 5)'}</MathJax.Node>
+    <p>
+      This is a little more subtle when we move into three dimensions. Just because
+      you have three vectors, it doesnt mean that they are all linearly independent.
+    </p>
+    <p>Obviously, you can get a case where they represent the same line</p>
+    <MathJaxMatrix inline matrix={[[1], [2], [3]]} />
+    <MathJaxMatrix inline matrix={[[2], [4], [6]]} />
+    <MathJaxMatrix inline matrix={[[-1], [-2], [-3]]} />
+    <Animation
+      initial={{ rotation: new Euler(0.5, 0.5, 0) }}
+      update={(state) => ({
+        rotation: new Euler(state.rotation.x,
+                            state.rotation.y + 0.001,
+                            state.rotation.z),
+      })}
+      render={(state) => (
+        <Visualization width={320} height={240} rotation={state.rotation}>
+          <XAxis />
+          <YAxis />
+          <ZAxis />
+          <Vector position={new Vector3(1, 2, 3)} color={0xffff00} />
+          <Vector position={new Vector3(2, 4, 6)} color={0xff00ff} />
+          <Vector position={new Vector3(-1, -2, -3)} color={0xffff} />
+        </Visualization>
+      )}
+    />
+    <p>But you might also get a case where they, represent the same plane</p>
+    <MathJaxMatrix inline matrix={[[1], [1], [1]]} />
+    <MathJaxMatrix inline matrix={[[1], [0], [1]]} />
+    <MathJaxMatrix inline matrix={[[0], [1], [0]]} />
+    <Animation
+      initial={{ rotation: new Euler(0.5, 0.5, 0) }}
+      update={(state) => ({
+        rotation: new Euler(state.rotation.x,
+                            state.rotation.y + 0.001,
+                            state.rotation.z),
+      })}
+      render={(state) => (
+        <Visualization width={320} height={240} rotation={state.rotation}>
+          <XAxis />
+          <YAxis />
+          <ZAxis />
+          <Vector position={new Vector3(1, 1, 1)} color={0xffff00} />
+          <Vector position={new Vector3(1, 0, 1)} color={0xff00ff} />
+          <Vector position={new Vector3(0, 1, 0)} color={0x00ffff} />
+          <mesh>
+            <parametricGeometry
+              parametricFunction={(u, v) =>
+                new Vector3(u,
+                            v,
+                            1)}
+              slices={1}
+              stacks={1}
+            />
+            <meshBasicMaterial color={0x009900} />
+          </mesh>
+        </Visualization>
+      )}
+    />
+    <p>
+      It is not immediately obvious, but adding the third vector to the
+      other two does not get you outside of the plane that the other
+      two are describing. So they are linearly dependent.
+    </p>
   </Section>
 );
 
