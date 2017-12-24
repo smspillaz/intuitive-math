3496d04f0c57ad2115a6fb47b99a3c541c3041a5
Sam Spilsbury <smspillaz@gmail.com>
2017-12-24 10:14:24 +0800

    animation: Don't run every animation all the time
    Only when we're actually visible

--- app/components/Animation/index.js
+++ app/components/Animation/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import TrackVisibility from 'react-on-screen';
+
 class Animation extends React.Component {
   static propTypes = {
     initial: PropTypes.any,
     update: PropTypes.func.isRequired,
     render: PropTypes.func.isRequired,
+    isVisible: PropTypes.bool.isRequired,
   };
 
   constructor(props) {
@@ -14,14 +17,53 @@ class Animation extends React.Component {
       ...props.initial,
     };
     this.frameId = -1;
+    this.animationCountdownId = -1;
   }
 
   componentDidMount() {
-    const updater = () => {
-      this.setState(this.props.update(this.state));
+    this.startAnimationCountdown();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.isVisible && this.props.isVisible) {
+      this.startAnimationCountdown();
+    } else if (prevProps.isVisible && !this.props.isVisible) {
+      this.stop();
+    }
+  }
+
+  componentWillUnmount() {
+    this.stop();
+  }
+
+  animate() {
+    if (this.frameId === -1 && this.props.isVisible) {
+      const updater = () => {
+        this.setState(this.props.update(this.state));
+        this.frameId = requestAnimationFrame(updater);
+      };
       this.frameId = requestAnimationFrame(updater);
-    };
-    this.frameId = requestAnimationFrame(updater);
+    }
+
+    this.animationCountdownId = -1;
+  }
+
+  startAnimationCountdown() {
+    if (this.animationCountdownId === -1) {
+      this.animationCountdownId = setTimeout(() => this.animate(), 200);
+    }
+  }
+
+  stop() {
+    if (this.frameId !== -1) {
+      cancelAnimationFrame(this.frameId);
+      this.frameId = -1;
+    }
+
+    if (this.animationCountdownId !== -1) {
+      clearTimeout(this.animationCountdownId);
+      this.animationCountdownId = -1;
+    }
   }
 
   render() {
@@ -29,4 +71,10 @@ class Animation extends React.Component {
   }
 }
 
-export default Animation;
+const PowerEfficientAnimation = (props) => (
+  <TrackVisibility offset={100}>
+    {({ isVisible }) => <Animation {...props} isVisible={isVisible} />}
+  </TrackVisibility>
+);
+
+export default PowerEfficientAnimation;
