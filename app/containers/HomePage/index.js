210485474324332ef5075bea709ce7c1e930ed56
Sam Spilsbury <smspillaz@gmail.com>
2017-12-24 16:04:03 +0800

    pages: Add HomePage content

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,6 +10,8 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
 
+import Section from 'components/Section';
+
 import injectReducer from 'utils/injectReducer';
 
 import reducer from './reducer';
@@ -23,7 +25,32 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
           <meta name="description" content="A React.js Boilerplate application homepage" />
         </Helmet>
         <div>
-          <p>Hello, world</p>
+          <Section title="Intuitive Math Primer" anchor="intuitive">
+            <p>Hello there,</p>
+            <p>
+              This is a math primer that is a little different. It is written
+              using technologies such as React and WebGL with animated explanations
+              of fields like Linear Algebra, designed to help you develop a
+              visual intuition.
+            </p>
+            <p>
+              I mainly wrote this to help me consolidate my own notes on the
+              subjects since I personally have a hard time instantly developing
+              an intuition for math. And without the intuition, I have a hard time
+              applying math to problems because I will not know what the tools
+              are actually for.
+            </p>
+            <p>
+              I hope you find it useful and feel free to
+              let me know if I got anything wrong or would like something to
+              be expanded in a little more detail at s@polysquare.org
+            </p>
+            <p>
+              If you are interested in how this was made, you can find the
+              source code for it on its{' '}
+              <a href="https://github.com/smspillaz/intuitive-math">GitHub repository</a>.
+            </p>
+          </Section>
         </div>
       </article>
     );
