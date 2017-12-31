1cd8b10f398809f7af8d01ba4585bc3e1012a513
Sam Spilsbury <smspillaz@gmail.com>
2017-12-31 09:33:22 +0800

    pages: Add section on row space

--- app/containers/LinearAlgebraPage/index.js
+++ app/containers/LinearAlgebraPage/index.js
@@ -1689,6 +1689,178 @@ const EROSection = () => (
   </Section>
 );
 
+const RowSpaceSection = () => (
+  <Section title="Row Space" anchor="row-space">
+    <p>
+      Given what we know about spans and matrices, the row space is just the
+      <Strong>span</Strong> of each of the rows, if we are to consider each
+      row to be a vector in a set.
+    </p>
+    <p>
+      Recall that the <Strong>span</Strong> is just the set of all linear
+      combinations of a set of vectors, which describes the space that is
+      reachable by those linear combinations.
+    </p>
+    <p>
+      So if you have a matrix defined like this:
+    </p>
+    <MathJaxMatrix matrix={[[1, 1], [2, 2]]} />
+    <BlankableVisualization width={320} height={240}>
+      <XAxis />
+      <YAxis />
+      <Vector color={0xffff00} position={new Vector3(1, 1, 0)} />
+      <Vector color={0xff00ff} position={new Vector3(1, 0, 1)} />
+    </BlankableVisualization>
+    <p>
+      Then because the rows are not linearly independent, the row space is
+      just going to be the line defined by <MathJax.Node>y = -x</MathJax.Node>
+    </p>
+    <p>
+      However, if you have a matrix defined with two linearly
+      independent vectors, then the row space is going to be all 2D space.
+    </p>
+    <MathJaxMatrix matrix={[[1, 1], [1, -1]]} />
+    <BlankableVisualization width={320} height={240}>
+      <XAxis />
+      <YAxis />
+      <Vector color={0xffff00} position={new Vector3(1, 1, 0)} />
+      <Vector color={0xff00ff} position={new Vector3(1, -1, 1)} />
+    </BlankableVisualization>
+    <p>
+      One thing which you might be interested in is finding the basis
+      for a row space. Recall that since all the vectors in a basis
+      must be linearly independent, the number of vectors in the basis
+      is going to tell you, at most, how many dimensions the space which
+      has the transformation applied to it, is going to have.
+    </p>
+    <p>
+      Thankfully, you do not have to do too much work to compute the
+      dimension of the row space. If you can immediately tell that the
+      rows are all lineraly independent from each other, then you know
+      that the row-space is n-dimensional, where n is the number of
+      rows in the matrix. Visually, this would mean that if you visualized
+      all the rows as planes with a solution of <MathJaxMatrix matrix={[[0], [0], [0]]} inline />,
+      then there would be a single point where they all intersect.
+    </p>
+    <p>
+      <MathJaxMatrix inline matrix={[[1, 1, 0], [1, 0, -1], [0, 1, -1]]} />
+      <MathJaxMatrix inline matrix={[['x'], ['y'], ['z']]} />
+      <MathJax.Node inline>=</MathJax.Node>
+      <MathJaxMatrix inline matrix={[[0], [0], [0]]} />
+    </p>
+    <Animation
+      initial={{
+        rotation: new Euler(0.5, 0.5, 0),
+      }}
+      update={(state) => ({
+        rotation: new Euler(state.rotation.x,
+                            state.rotation.y + 0.004,
+                            state.rotation.z),
+      })}
+      render={(state) => (
+        <Visualization width={320} height={240} rotation={state.rotation}>
+          <XAxis />
+          <YAxis />
+          <ZAxis />
+          <Plane extents={[-2, 2]} a={1} b={1} c={0} d={0} color={0xffff00} transparent opacity={0.8} />
+          <Plane extents={[-2, 2]} a={1} b={0} c={-1} d={0} color={0xff00ff} transparent opacity={0.8} />
+          <Plane extents={[-2, 2]} a={0} b={1} c={-1} d={0} color={0x00ffff} transparent opacity={0.8} />
+        </Visualization>
+      )}
+    />
+    <p>
+      If you want to more rigorously prove what the dimension of the row space is,
+      you can use <Strong>Elementary Row Operations</Strong> as explained above. Recall
+      that since we are only interested in finding the set of all possible vectors
+      spanned by the rows and that either our row space consisted of linearly independent
+      vectors or linearly dependent vectors and all <Strong>Elementary Row Operations</Strong>
+      actually do is recover the standard basis vectors through linear combinations, it follows
+      that performing such operations are safe, in that they will not change the dimension,
+      which is what we are looking for.
+    </p>
+    <p>
+      With that mouthful out of the way, recall that the matrix above
+      row-reduced to:
+    </p>
+    <MathJaxMatrix matrix={[[1, 0, 0], [0, 1, 0], [0, 0, 1]]} />
+    <Animation
+      initial={{
+        rotation: new Euler(0.5, 0.5, 0),
+      }}
+      update={(state) => ({
+        rotation: new Euler(state.rotation.x,
+                            state.rotation.y + 0.004,
+                            state.rotation.z),
+      })}
+      render={(state) => (
+        <Visualization width={320} height={240} rotation={state.rotation}>
+          <XAxis />
+          <YAxis />
+          <ZAxis />
+          <Plane extents={[-2, 2]} a={1} b={0} c={0} d={0} color={0xffff00} transparent opacity={0.8} />
+          <Plane extents={[-2, 2]} a={0} b={1} c={0} d={0} color={0xff00ff} transparent opacity={0.8} />
+          <Plane extents={[-2, 2]} a={0} b={0} c={1} d={0} color={0x00ffff} transparent opacity={0.8} />
+        </Visualization>
+      )}
+    />
+    <p>
+      So, given that we have three vectors that are linearly independent, our
+      transformation has at most three dimensions.
+    </p>
+    <p>
+      The row space might not always have as many dimensions as the number of
+      rows in the matrix. For instance, consider the matrix:
+    </p>
+    <MathJaxMatrix matrix={[[1, 1, 0], [2, 2, 0], [0, 0, 1]]} />
+    <Animation
+      initial={{
+        rotation: new Euler(0.5, 0.5, 0),
+      }}
+      update={(state) => ({
+        rotation: new Euler(state.rotation.x,
+                            state.rotation.y + 0.004,
+                            state.rotation.z),
+      })}
+      render={(state) => (
+        <Visualization width={320} height={240} rotation={state.rotation}>
+          <XAxis />
+          <YAxis />
+          <ZAxis />
+          <Plane extents={[-2, 2]} a={1} b={1} c={0} d={0} color={0xffff00} transparent opacity={0.8} />
+          <Plane extents={[-2, 2]} a={2} b={2} c={0} d={0} color={0xff00ff} transparent opacity={0.8} />
+          <Plane extents={[-2, 2]} a={0} b={0} c={1} d={0} color={0x00ffff} transparent opacity={0.8} />
+        </Visualization>
+      )}
+    />
+    <p>
+      With this matrix, we can immediately tell that the second row has a
+      linear dependence on the first (it is just a scalar multiple of it). Indeed,
+      it row-reduces to:
+    </p>
+    <MathJaxMatrix matrix={[[1, 1, 0], [0, 0, 0], [0, 0, 1]]} />
+    <Animation
+      initial={{
+        rotation: new Euler(0.5, 0.5, 0),
+      }}
+      update={(state) => ({
+        rotation: new Euler(state.rotation.x,
+                            state.rotation.y + 0.004,
+                            state.rotation.z),
+      })}
+      render={(state) => (
+        <Visualization width={320} height={240} rotation={state.rotation}>
+          <XAxis />
+          <YAxis />
+          <ZAxis />
+          <Plane extents={[-2, 2]} a={1} b={1} c={0} d={0} color={0xffff00} transparent opacity={0.8} />
+          <Plane extents={[-2, 2]} a={0} b={0} c={0} d={0} color={0xff00ff} transparent opacity={0.8} />
+          <Plane extents={[-2, 2]} a={0} b={0} c={1} d={0} color={0x00ffff} transparent opacity={0.8} />
+        </Visualization>
+      )}
+    />
+  </Section>
+);
+
 export class LinearAlgebraPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
@@ -1708,6 +1880,7 @@ export class LinearAlgebraPage extends React.PureComponent { // eslint-disable-l
               <SpansSection />
               <BasisSection />
               <EROSection />
+              <RowSpaceSection />
             </div>
           </MathJax.Context>
         </div>
