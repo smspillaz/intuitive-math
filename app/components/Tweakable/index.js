dd472b00c32fb5572516de2cbea6d94d66310c3a
Sam Spilsbury <smspillaz@gmail.com>
2018-02-13 23:47:48 +0800

    Tweakable: Add Tweakable component
    This component is designed to tie in with the props passed for
    each variable in InterpolatedAnimation, where we have a visualization
    of a value that is "tweakable" in the sense that it can be edited
    by dragging the mouse cursor. The editor will be appended to the
    end of the children of the element.

---
+++ app/components/Tweakable/index.js
@@ -0,0 +1,49 @@
+/*
+ * Tweakable
+ *
+ * A component to tweak some underlying span by clicking and dragging on it.
+ */
+
+import React from 'react';
+
+import styled from 'styled-components';
+
+import PropTypes from 'prop-types';
+
+import EditorCloseIcon from '@atlaskit/icon/glyph/editor/close';
+import NumberEditor from 'react-number-editor';
+
+const CenteredGlyphText = styled.span`
+  text-align: center;
+`;
+
+const Tweakable = ({ begin, end, value, onChange, onClear, overridden, children, step = 0.01 }) => (
+  <CenteredGlyphText>
+    {children}
+    <NumberEditor
+      min={begin}
+      max={end}
+      value={value}
+      decimals={2}
+      onValueChange={(v) => onChange(Number(v))}
+      step={step}
+      style={{
+        width: `${Math.max(String(value.toFixed(2)).length, 5) * 8}px`,
+      }}
+    />
+    {overridden ? <EditorCloseIcon onClick={onClear} size="small" /> : <span />}
+  </CenteredGlyphText>
+);
+
+Tweakable.propTypes = {
+  begin: PropTypes.number.isRequired,
+  end: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onClear: PropTypes.func.isRequired,
+  overridden: PropTypes.bool.isRequired,
+  children: PropTypes.element,
+  step: PropTypes.number,
+};
+
+export default Tweakable;
