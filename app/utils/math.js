a94c73f89f1f2dad5d51e1a211b0d6fa19c11ff4
Sam Spilsbury <smspillaz@gmail.com>
2018-02-14 13:22:20 +0800

    math: Move Plane math calculations to utils/math

--- app/utils/math.js
+++ app/utils/math.js
@@ -8,3 +8,18 @@ export const truncate = (num, precision) => {
   const power = 10 ** precision;
   return Math.floor(num * power) / power;
 };
+
+export const cross = (vec1, vec2) => (
+  [
+    (vec1[1] * vec2[2]) - (vec1[2] * vec2[1]),
+    (vec1[2] * vec2[0]) - (vec1[0] * vec2[2]),
+    (vec1[0] * vec2[1]) - (vec1[1] * vec2[0]),
+  ]
+);
+
+export const tangentVectors = ([a, b, c]) => {
+  if (a === 0 && b === 0) {
+    return [[1, 0, 0], [0, 1, 0]];
+  }
+  return [[b, -a, 0], cross([b, -a, 0], [a, b, c])];
+};
