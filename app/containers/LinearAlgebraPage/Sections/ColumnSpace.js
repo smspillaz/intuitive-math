939ba76a419989f18bf6fa82f61acbd9e48b58f8
Sam Spilsbury <smspillaz@gmail.com>
2018-02-14 16:11:53 +0800

    ColumnSpace: Use AxisVisualization, TriplePlanes, etc

--- app/containers/LinearAlgebraPage/Sections/ColumnSpace.js
+++ app/containers/LinearAlgebraPage/Sections/ColumnSpace.js
@@ -8,16 +8,17 @@ import React from 'react';
 
 import MathJax from 'react-mathjax';
 
-import { Euler, Matrix3, Vector3 } from 'three';
+import { Matrix3, Vector3 } from 'three';
 
-import { XAxis, YAxis, ZAxis } from 'components/Axis';
-import Animation from 'components/Animation';
+import AxisVisualization2D from 'components/AxisVisualization2D';
+import AxisVisualization3D from 'components/AxisVisualization3D';
+import InterpolatedAnimation from 'components/InterpolatedAnimation';
 import MathJaxMatrix from 'components/MathJaxMatrix';
 import Plane from 'components/Plane';
 import Section from 'components/Section';
 import Strong from 'components/Strong';
+import TriplePlanes from 'components/TriplePlanes';
 import Vector from 'components/Vector';
-import Visualization from 'components/Visualization';
 
 const ColumnSpaceSection = () => (
   <Section title="Column Space" anchor="column-space">
@@ -39,33 +40,33 @@ const ColumnSpaceSection = () => (
       the standard basis vector <MathJax.Node inline>\hat j</MathJax.Node> lands on{' '}
       <MathJaxMatrix inline matrix={[[1], [1]]} />.
     </p>
-    <Animation
-      initial={{ time: 0 }}
-      update={(state) => ({
-        time: state.time + 1,
-      })}
-      render={(state) => {
-        const lerp = Math.max(Math.sin(state.time * 0.05) + 1, 0) / 2;
+    <InterpolatedAnimation
+      values={{
+        xxInterp: { begin: 1, end: 3 },
+        xyInterp: { begin: 0, end: 1 },
+        yxInterp: { begin: 0, end: 1 },
+      }}
+      render={({ xxInterp, xyInterp, yxInterp }) => {
         const mat = new Matrix3();
         const iHat = new Vector3(1, 0, 0);
         const jHat = new Vector3(0, 1, 0);
 
-        mat.set(1 + (2 * lerp), (1 * lerp), 0,
-                lerp, 1, 0,
+        mat.set(xxInterp.value, xyInterp.value, 0,
+                yxInterp.value, 1, 0,
                 0, 0, 0);
 
         const transformedIHat = iHat.clone().applyMatrix3(mat);
         const transformedJHat = jHat.clone().applyMatrix3(mat);
 
         return (
-          <div>
-            <Visualization width={320} height={240}>
-              <XAxis />
-              <YAxis />
-              <Vector position={transformedIHat} color={0xffff00} />
-              <Vector position={transformedJHat} color={0xffff00} />
-            </Visualization>
-          </div>
+          <AxisVisualization2D
+            render={() => (
+              <group>
+                <Vector position={transformedIHat} color={0xffff00} />
+                <Vector position={transformedJHat} color={0xffff00} />
+              </group>
+            )}
+          />
         );
       }}
     />
@@ -97,25 +98,10 @@ const ColumnSpaceSection = () => (
       <MathJax.Node inline>\rightarrow</MathJax.Node>
       <MathJaxMatrix inline matrix={[[1, 1, 2], [2, 1, 1], [3, 3, 6]]} />
     </p>
-    <Animation
-      initial={{
-        rotation: new Euler(0.5, 0.5, 0),
-      }}
-      update={(state) => ({
-        rotation: new Euler(state.rotation.x,
-                            state.rotation.y + 0.004,
-                            state.rotation.z),
-      })}
-      render={(state) => (
-        <Visualization width={320} height={240} rotation={state.rotation}>
-          <XAxis />
-          <YAxis />
-          <ZAxis />
-          <Plane extents={[-1, 1]} a={1} b={1} c={2} d={0} color={0xffff00} transparent opacity={0.8} />
-          <Plane extents={[-1, 1]} a={1} b={1} c={3} d={0} color={0xff00ff} transparent opacity={0.8} />
-          <Plane extents={[-1, 1]} a={2} b={1} c={6} d={0} color={0x00ffff} transparent opacity={0.8} />
-        </Visualization>
-      )}
+    <TriplePlanes
+      first={[1, 1, 2, 0]}
+      second={[1, 1, 3, 0]}
+      third={[2, 1, 6, 0]}
     />
     <p>
       Now we can row-reduce as usual:
@@ -125,25 +111,10 @@ const ColumnSpaceSection = () => (
       {'~'}
       <MathJaxMatrix inline matrix={[[-1, 0, 1], [0, 1, 3], [0, 0, 0]]} />
     </p>
-    <Animation
-      initial={{
-        rotation: new Euler(0.5, 0.5, 0),
-      }}
-      update={(state) => ({
-        rotation: new Euler(state.rotation.x,
-                            state.rotation.y + 0.004,
-                            state.rotation.z),
-      })}
-      render={(state) => (
-        <Visualization width={320} height={240} rotation={state.rotation}>
-          <XAxis />
-          <YAxis />
-          <ZAxis />
-          <Plane extents={[-1, 1]} a={-1} b={0} c={1} d={0} color={0xffff00} transparent opacity={0.8} />
-          <Plane extents={[-1, 1]} a={0} b={1} c={3} d={0} color={0xff00ff} transparent opacity={0.8} />
-          <Plane extents={[-1, 1]} a={0} b={0} c={0} d={0} color={0x00ffff} transparent opacity={0.8} />
-        </Visualization>
-      )}
+    <TriplePlanes
+      first={[-1, 0, 1, 0]}
+      second={[0, 1, 3, 0]}
+      third={[0, 0, 0, 0]}
     />
     <p>
       Transposing our row-reduced matrix, we get a new matrix with our column space.
@@ -157,46 +128,24 @@ const ColumnSpaceSection = () => (
       So our column space here comprises of the vectors <MathJaxMatrix inline matrix={[[-1], [0], [1]]} />{' '}
       and <MathJaxMatrix inline matrix={[[0], [1], [3]]} />{' '}
     </p>
-    <Animation
-      initial={{
-        rotation: new Euler(0.5, 0.5, 0),
-      }}
-      update={(state) => ({
-        rotation: new Euler(state.rotation.x,
-                            state.rotation.y + 0.004,
-                            state.rotation.z),
-      })}
-      render={(state) => (
-        <Visualization width={320} height={240} rotation={state.rotation}>
-          <XAxis />
-          <YAxis />
-          <ZAxis />
+    <AxisVisualization3D
+      render={() => (
+        <group>
           <Vector position={new Vector3(-1, 0, 0)} color={0xff00ff} />
           <Vector position={new Vector3(0, 1, 3)} color={0xffff00} />
-        </Visualization>
+        </group>
       )}
     />
     <p>
       Which, you will notice, forms a plane, indicating that our output space is two dimensional.
     </p>
-    <Animation
-      initial={{
-        rotation: new Euler(0.5, 0.5, 0),
-      }}
-      update={(state) => ({
-        rotation: new Euler(state.rotation.x,
-                            state.rotation.y + 0.004,
-                            state.rotation.z),
-      })}
-      render={(state) => (
-        <Visualization width={320} height={240} rotation={state.rotation}>
-          <XAxis />
-          <YAxis />
-          <ZAxis />
-          <Vector position={new Vector3(-1, 0, 0)} color={0xffff00} />
-          <Vector position={new Vector3(0, 1, 3)} color={0xff00ff} />
+    <AxisVisualization3D
+      render={() => (
+        <group>
+          <Vector position={new Vector3(-1, 0, 0)} color={0xff00ff} />
+          <Vector position={new Vector3(0, 1, 3)} color={0xffff00} />
           <Plane extents={[-1, 1]} a={0} b={-3} c={1} d={0} color={0x00ffff} transparent opacity={0.8} />
-        </Visualization>
+        </group>
       )}
     />
   </Section>
