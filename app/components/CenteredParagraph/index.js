2dd587a396cac8d2b98d861f8aea7893d5dca380
Sam Spilsbury <smspillaz@gmail.com>
2018-02-15 22:34:13 +0800

    components: Add CenteredParagraph

---
+++ app/components/CenteredParagraph/index.js
@@ -0,0 +1,13 @@
+/*
+ * CenteredParagraph
+ *
+ * A paragraph, but centered.
+ */
+
+import styled from 'styled-components';
+
+const CenteredParagraph = styled.p`
+  text-align: center;
+`;
+
+export default CenteredParagraph;
