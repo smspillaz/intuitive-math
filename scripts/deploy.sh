e52bfe76ffac12a6fa34117f360ed4a1b8f91f47
Sam Spilsbury <smspillaz@gmail.com>
2018-02-22 10:18:50 +0800

    scripts: Also write config file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# We'll need to set up our credentials
+echo <<< EOL
+credentials:
+  accessKeyId: ${AWS_ACCESS_KEY_ID}
+  secretAccessKey: ${AWS_SECRET_ACCESS_KEY}
+development:
+  NODE_ENV: production
+staging:
+  NODE_ENV: production
+production:
+  NODE_ENV: production
+EOL >> ../intuimath-config/config/app/env.yml
+
 yarn install
 yarn build:dll
 yarn build
