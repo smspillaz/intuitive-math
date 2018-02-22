dfdf7f6397f70a0280c2e51e9e5e65689345ba39
Sam Spilsbury <smspillaz@gmail.com>
2018-02-22 22:14:42 +0800

    package: babelify server and internals too

--- babel.config.js
+++ babel.config.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   env: {
     production: {
-      only: ['app'],
+      only: ['app', 'internals', 'server'],
       plugins: [
         'lodash',
         'transform-react-remove-prop-types',
