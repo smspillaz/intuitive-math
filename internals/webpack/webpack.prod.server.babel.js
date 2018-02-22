1de00b8a95d20c88f24b966d476b8048cb14b39b
Sam Spilsbury <smspillaz@gmail.com>
2018-02-22 22:13:48 +0800

    webpack: Build client and server bundles

---
+++ internals/webpack/webpack.prod.server.babel.js
@@ -0,0 +1,19 @@
+// Important modules this config uses
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = require('./webpack.prod.babel')({
+  // In production, we skip all hot-reloading stuff
+  entry: [
+    path.join(process.cwd(), 'server/index.js'),
+  ],
+
+  externals: [nodeExternals()],
+
+  output: {
+    filename: 'prodServer.js',
+    path: path.join(process.cwd(), 'build'),
+  },
+
+  target: 'node',
+});
