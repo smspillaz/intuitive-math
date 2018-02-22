c42151dcde93164ade1d9df8a3cf5983dff9a069
Sam Spilsbury <smspillaz@gmail.com>
2018-02-22 22:44:47 +0800

    webpack: Pass through libraryTarget and library
    This can be used for the serverless build later when we want to
    export a library

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -13,6 +13,8 @@ module.exports = options => ({
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
       publicPath: '/',
+      libraryTarget: options.libraryTarget,
+      library: options.library,
     },
     options.output,
   ), // Merge with env dependent settings
