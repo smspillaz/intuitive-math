300a6c7e667a47d5a5745326e306c854ee81b446
Sam Spilsbury <smspillaz@gmail.com>
2018-02-22 22:45:28 +0800

    lambda: Tidy up style for exporting

--- lambda.js
+++ lambda.js
@@ -1,12 +1,14 @@
 const serverless = require('serverless-http');
 const app = require('./server/server');
 
-exports.handler = serverless(app, {
-  binary: [
-    'font/eot',
-    'font/opentype',
-    'font/otf',
-    'image/jpeg',
-    'image/png',
-  ],
-});
+exports.handler = {
+  handler: serverless(app, {
+    binary: [
+      'font/eot',
+      'font/opentype',
+      'font/otf',
+      'image/jpeg',
+      'image/png',
+    ],
+  }),
+};
