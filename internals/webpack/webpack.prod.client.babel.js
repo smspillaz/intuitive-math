b250031a5bc10d90020f50650d9aab1600e28c24
Sam Spilsbury <smspillaz@gmail.com>
2018-02-23 16:23:48 +0800

    app: Split client entry point out into a separate file
    This will allow us to import the app code from the server side
    renderer

--- internals/webpack/webpack.prod.client.babel.js
+++ internals/webpack/webpack.prod.client.babel.js
@@ -8,7 +8,7 @@ const { HashedModuleIdsPlugin } = require('webpack');
 const config = require('./webpack.prod.babel')({
   // In production, we skip all hot-reloading stuff
   entry: [
-    path.join(process.cwd(), 'app/app.js'),
+    path.join(process.cwd(), 'app/client.js'),
   ],
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
