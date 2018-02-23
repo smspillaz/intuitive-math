5ce4821f0075e7e030713e2e40ab48fdc6d9d691
Sam Spilsbury <smspillaz@gmail.com>
2018-02-23 16:15:05 +0800

    package: Add __SERVER__ to globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,9 @@ module.exports = {
       jsx: true,
     },
   },
+  globals: {
+    __SERVER__: true,
+  },
   rules: {
     'prettier/prettier': ['error', prettierOptions],
     'arrow-body-style': [2, 'as-needed'],
