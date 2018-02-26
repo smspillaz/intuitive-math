b3dd983ff5882831a0bf874f3fd503532d8ea976
Sam Spilsbury <smspillaz@gmail.com>
2018-02-26 08:47:26 +0800

    app: Hydrate redux state on client too

--- app/client.js
+++ app/client.js
@@ -50,8 +50,9 @@ openSansObserver.load().then(
   },
 );
 
-// Create redux store with history
-const initialState = {};
+// Create redux store with history and hydrate state from server, if available
+// eslint-disable-next-line no-underscore-dangle
+const initialState = window.__SERVER_STATE || {};
 const history = createHistory();
 const store = configureStore(initialState, history);
 
