0abd7ca70a9fecad35767bd7a4ac10f1adcba31d
Sam Spilsbury <smspillaz@gmail.com>
2018-03-02 09:15:28 +0800

    images: Adjust favicon based on asset from jackrobertscott

--- app/client.js
+++ app/client.js
@@ -16,17 +16,13 @@ import FontFaceObserver from 'fontfaceobserver';
 // Load the favicon, the manifest.json file and the .htaccess file
 /* eslint-disable import/no-webpack-loader-syntax */
 import '!file-loader?name=[name].[ext]!./images/favicon.ico';
-import '!file-loader?name=[name].[ext]!./images/icon-72x72.png';
-import '!file-loader?name=[name].[ext]!./images/icon-96x96.png';
-import '!file-loader?name=[name].[ext]!./images/icon-120x120.png';
-import '!file-loader?name=[name].[ext]!./images/icon-128x128.png';
-import '!file-loader?name=[name].[ext]!./images/icon-144x144.png';
-import '!file-loader?name=[name].[ext]!./images/icon-152x152.png';
-import '!file-loader?name=[name].[ext]!./images/icon-167x167.png';
-import '!file-loader?name=[name].[ext]!./images/icon-180x180.png';
-import '!file-loader?name=[name].[ext]!./images/icon-192x192.png';
-import '!file-loader?name=[name].[ext]!./images/icon-384x384.png';
-import '!file-loader?name=[name].[ext]!./images/icon-512x512.png';
+import '!file-loader?name=[name].[ext]!./images/favicon-16x16.png';
+import '!file-loader?name=[name].[ext]!./images/favicon-32x32.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-192x192.png';
+import '!file-loader?name=[name].[ext]!./images/android-chrome-512x512.png';
+import '!file-loader?name=[name].[ext]!./images/mstile-150x150.png';
+import '!file-loader?name=[name].[ext]!./images/apple-touch-icon.png';
+import '!file-loader?name=[name].[ext]!./images/safari-pinned-tab.svg';
 import '!file-loader?name=[name].[ext]!./manifest.json';
 import 'file-loader?name=[name].[ext]!./.htaccess'; // eslint-disable-line import/extensions
 /* eslint-enable import/no-webpack-loader-syntax */
