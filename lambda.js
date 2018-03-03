74ef23fb57b6ace3cf21c4adc6c1d063a393b921
Sam Spilsbury <smspillaz@gmail.com>
2018-03-03 09:10:47 +0800

    lambda: Also add image/x-icon to binary types

--- lambda.js
+++ lambda.js
@@ -3,6 +3,6 @@ const app = require('./server/server');
 
 module.exports = {
   handler: serverless(app, {
-    binary: ['image/png', 'image/jpeg'],
+    binary: ['image/png', 'image/jpeg', 'image/x-icon'],
   }),
 };
