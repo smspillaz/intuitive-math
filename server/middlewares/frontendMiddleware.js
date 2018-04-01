2d896e5661bff2d099dc4c446e3b21930240a400
Sam Spilsbury <smspillaz@gmail.com>
2018-04-01 10:24:40 +0800

    server: Redirect requests for /sw.js to /static

--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -1,10 +1,16 @@
 /* eslint-disable global-require */
 
+const redirect = (dst) => (req, res) => res.redirect(dst);
+
 /**
  * Front-end middleware
  */
 module.exports = (app, options) => {
   const isProd = process.env.NODE_ENV === 'production';
+
+  // redirect favicons and service workers
+  app.get('/sw.js', redirect('/static/sw.js'));
+  app.get('/favicon.ico', redirect('/static/favicon.ico'));
 
   if (isProd) {
     const addProdMiddlewares = require('./addProdMiddlewares');
