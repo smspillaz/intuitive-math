01bd3ac9cc7c18e99a937099cf38d51dcef8445b
Sam Spilsbury <smspillaz@gmail.com>
2018-04-01 16:26:36 +0800

    components: Add Pillar component
    We'll use this to represent area and volume integrations

---
+++ app/components/Pillar/index.js
@@ -0,0 +1,61 @@
+/*
+ * Pillar
+ *
+ * A component which is a single pillar, centered on the axis.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Vector3 } from 'three';
+
+const Pillar = ({
+  width,
+  height,
+  depth,
+  x,
+  y,
+  z,
+  color,
+  opacity = 0.8,
+  wireframe = false,
+}) => {
+  const volume = width * height * depth;
+
+  if (volume === 0) {
+    return (<group />);
+  }
+
+  const xPosition = x + (width / 2);
+  const yPosition = y + (height / 2);
+  const zPosition = z + (depth / 2);
+  const position = new Vector3(xPosition, yPosition, zPosition);
+
+  return (
+    <group>
+      <mesh position={position}>
+        <boxGeometry width={width} height={height} depth={depth} />
+        <meshBasicMaterial
+          color={color}
+          opacity={opacity}
+          transparent
+          wireframe={wireframe}
+        />
+      </mesh>
+    </group>
+  );
+};
+
+Pillar.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  depth: PropTypes.number.isRequired,
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  z: PropTypes.number.isRequired,
+  color: PropTypes.number.isRequired,
+  opacity: PropTypes.number,
+  wireframe: PropTypes.boolean,
+};
+
+export default Pillar;
