845b0bd6ef23fd3aed8d3664374de96ae1578b0c
Sam Spilsbury <smspillaz@gmail.com>
2018-04-01 16:28:50 +0800

    DobuleIntegralVisualization: Used to visualize a double-integral over area

---
+++ app/components/DoubleIntegralVisualization/index.js
@@ -0,0 +1,71 @@
+/*
+ * SingleIntegralVisualization
+ *
+ * A visualization for a double integral (usually 3D space).
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import AxisVisualization3D from 'components/AxisVisualization3D';
+import Pillar from 'components/Pillar';
+
+import { segment1D } from 'utils/math';
+
+const DoubleIntegralVisualization = ({
+  xMin,
+  xMax,
+  yMin,
+  yMax,
+  zMin,
+  zMax,
+  func,
+  segments,
+  opacityFunc = () => 0.8,
+  wireframe = true,
+}) => (
+  <AxisVisualization3D
+    render={() => (
+      <group>
+        {segment1D(xMin, xMax, segments).map(([xBegin, xEnd]) => {
+          const xCenter = xBegin + ((xEnd - xBegin) / 2);
+          return segment1D(yMin, yMax, segments).map(([yBegin, yEnd]) => {
+            const yCenter = yBegin + ((yEnd - yBegin) / 2);
+            const value = func(xCenter, yCenter);
+            const depth = Math.max(Math.max(Math.min(zMax, 0) + Math.min(0, value), value) - zMin, 0);
+            const opacity = opacityFunc(xCenter, yCenter);
+            return (
+              <Pillar
+                key={`${xCenter}-${yCenter}`}
+                width={(xEnd - xBegin)}
+                height={yEnd - yBegin}
+                depth={depth}
+                x={xBegin}
+                y={yBegin}
+                z={zMin}
+                color={0xff00ff}
+                opacity={opacity}
+                wireframe={wireframe}
+              />
+            );
+          });
+        })}
+      </group>
+    )}
+  />
+);
+
+DoubleIntegralVisualization.propTypes = {
+  xMin: PropTypes.number.isRequired,
+  xMax: PropTypes.number.isRequired,
+  yMin: PropTypes.number.isRequired,
+  yMax: PropTypes.number.isRequired,
+  zMin: PropTypes.number.isRequired,
+  zMax: PropTypes.number.isRequired,
+  func: PropTypes.func.isRequired,
+  opacityFunc: PropTypes.func,
+  segments: PropTypes.number.isRequired,
+  wireframe: PropTypes.boolean,
+};
+
+export default DoubleIntegralVisualization;
