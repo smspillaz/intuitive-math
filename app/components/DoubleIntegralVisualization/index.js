6abc2b41283e19c3bd08615936cea96c0fe4e366
Sam Spilsbury <smspillaz@gmail.com>
2018-04-07 15:33:14 +0800

    DoubleIntegralVisualization: Split out DoubleIntegral

--- app/components/DoubleIntegralVisualization/index.js
+++ app/components/DoubleIntegralVisualization/index.js
@@ -12,7 +12,7 @@ import Pillar from 'components/Pillar';
 
 import { segment1D } from 'utils/math';
 
-const DoubleIntegralVisualization = ({
+export const DoubleIntegral = ({
   xMin,
   xMax,
   yMin,
@@ -24,33 +24,50 @@ const DoubleIntegralVisualization = ({
   opacityFunc = () => 0.8,
   wireframe = true,
 }) => (
+  <group>
+    {segment1D(xMin, xMax, segments).map(([xBegin, xEnd]) => {
+      const xCenter = xBegin + ((xEnd - xBegin) / 2);
+      return segment1D(yMin, yMax, segments).map(([yBegin, yEnd]) => {
+        const yCenter = yBegin + ((yEnd - yBegin) / 2);
+        const value = func(xCenter, yCenter);
+        const depth = Math.max(Math.max(Math.min(zMax, 0) + Math.min(0, value), value) - zMin, 0);
+        const opacity = opacityFunc(xCenter, yCenter);
+        return (
+          <Pillar
+            key={`${xCenter}-${yCenter}`}
+            width={(xEnd - xBegin)}
+            height={yEnd - yBegin}
+            depth={depth}
+            x={xBegin}
+            y={yBegin}
+            z={zMin}
+            color={0xff00ff}
+            opacity={opacity}
+            wireframe={wireframe}
+          />
+        );
+      });
+    })}
+  </group>
+);
+
+DoubleIntegral.propTypes = {
+  xMin: PropTypes.number.isRequired,
+  xMax: PropTypes.number.isRequired,
+  yMin: PropTypes.number.isRequired,
+  yMax: PropTypes.number.isRequired,
+  zMin: PropTypes.number.isRequired,
+  zMax: PropTypes.number.isRequired,
+  func: PropTypes.func.isRequired,
+  opacityFunc: PropTypes.func,
+  segments: PropTypes.number.isRequired,
+  wireframe: PropTypes.boolean,
+};
+
+const DoubleIntegralVisualization = (props) => (
   <AxisVisualization3D
     render={() => (
-      <group>
-        {segment1D(xMin, xMax, segments).map(([xBegin, xEnd]) => {
-          const xCenter = xBegin + ((xEnd - xBegin) / 2);
-          return segment1D(yMin, yMax, segments).map(([yBegin, yEnd]) => {
-            const yCenter = yBegin + ((yEnd - yBegin) / 2);
-            const value = func(xCenter, yCenter);
-            const depth = Math.max(Math.max(Math.min(zMax, 0) + Math.min(0, value), value) - zMin, 0);
-            const opacity = opacityFunc(xCenter, yCenter);
-            return (
-              <Pillar
-                key={`${xCenter}-${yCenter}`}
-                width={(xEnd - xBegin)}
-                height={yEnd - yBegin}
-                depth={depth}
-                x={xBegin}
-                y={yBegin}
-                z={zMin}
-                color={0xff00ff}
-                opacity={opacity}
-                wireframe={wireframe}
-              />
-            );
-          });
-        })}
-      </group>
+      <DoubleIntegral {...props} />
     )}
   />
 );
