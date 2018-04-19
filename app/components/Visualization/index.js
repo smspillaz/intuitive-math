b22684b3c25a0faa17de626ba891ccd0239e9b73
Sam Spilsbury <smspillaz@gmail.com>
2018-04-19 22:34:11 +0800

    Visualization: Make ClickToAnimate a parent of PlayableVisualization
    But nothing else

--- app/components/Visualization/index.js
+++ app/components/Visualization/index.js
@@ -463,18 +463,18 @@ const Visualization = ({
   renderExtras,
   ...props
 }) => (
-  <ClickToAnimate>
-    <OverlayTweenFromVisibility
-      fadeTime={fadeTime}
-      isVisible={isVisible}
-      render={({ overlayOpacity }) => (
-        <Centered>
-          <TweenOverlayVisualization
-            width={width}
-            height={height}
-            overlayOpacity={overlayOpacity}
-            curvedBottomCorners={!renderExtras}
-          >
+  <OverlayTweenFromVisibility
+    fadeTime={fadeTime}
+    isVisible={isVisible}
+    render={({ overlayOpacity }) => (
+      <Centered>
+        <TweenOverlayVisualization
+          width={width}
+          height={height}
+          overlayOpacity={overlayOpacity}
+          curvedBottomCorners={!renderExtras}
+        >
+          <ClickToAnimate>
             <PlayableVisualization
               width={width}
               height={height}
@@ -484,12 +484,12 @@ const Visualization = ({
             >
               {children}
             </PlayableVisualization>
-          </TweenOverlayVisualization>
-          {renderExtras && renderExtras({ width, animationIsRunning })}
-        </Centered>
-      )}
-    />
-  </ClickToAnimate>
+          </ClickToAnimate>
+        </TweenOverlayVisualization>
+        {renderExtras && renderExtras({ width, animationIsRunning })}
+      </Centered>
+    )}
+  />
 );
 
 Visualization.propTypes = {
