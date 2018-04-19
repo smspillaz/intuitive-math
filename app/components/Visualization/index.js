c38464a3fe94d6462e6c9fc4e270cd05ff92a804
Sam Spilsbury <smspillaz@gmail.com>
2018-04-19 08:54:56 +0800

    Visualization: Make sure TweakablesBox size is in px

--- app/components/Visualization/index.js
+++ app/components/Visualization/index.js
@@ -251,7 +251,7 @@ const TweakablesBoxBorder = styled.div`
   border-style: solid;
   display: inline-block;
   padding: 20px;
-  width: ${(props) => props.width}
+  width: ${(props) => props.width}px;
 `;
 
 TweakablesBoxBorder.propTypes = {
