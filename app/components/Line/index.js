ab8e44e3552845f6b111ce30678ceade2c88cf59
Sam Spilsbury <smspillaz@gmail.com>
2018-10-16 23:00:49 +0300

    Line: Reimplement in terms of ThreeLine

--- app/components/Line/index.js
+++ app/components/Line/index.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Vector3 } from 'three';
+import THREE, { Geometry, LineBasicMaterial, Vector3 } from 'three';
+
+import { Group, asSceneElement, constructConstructorlessThreeObject } from 'components/Visualization';
+
+const ThreeLine = asSceneElement(
+  {
+    material: PropTypes.object.isRequired,
+    geometry: PropTypes.object.isRequired,
+  },
+  props => constructConstructorlessThreeObject(THREE.Line, props)
+);
 
 const Line = ({ basis, extents, color, offset = [0, 0, 0] }) => (
-  <line>
-    <geometry
-      vertices={[
-        new Vector3((basis[0] * extents[0]) + offset[0],
-                    (basis[1] * extents[0]) + offset[1],
-                    (basis[2] * extents[0]) + offset[2]),
-        new Vector3((basis[0] * extents[1]) + offset[0],
-                    (basis[1] * extents[1]) + offset[1],
-                    (basis[2] * extents[1]) + offset[2]),
-      ]}
+  <Group>
+    <ThreeLine
+      material={new LineBasicMaterial({ color })}
+      geometry={(() => {
+        const geometry = new Geometry();
+
+        geometry.vertices.push(
+          new Vector3(
+            basis[0] * extents[0] + offset[0],
+            basis[1] * extents[0] + offset[1],
+            basis[2] * extents[0] + offset[2]),
+          new Vector3(
+            basis[0] * extents[1] + offset[0],
+            basis[1] * extents[1] + offset[1],
+            basis[2] * extents[1] + offset[2]
+          ),
+        );
+      })()}
     />
-    <lineBasicMaterial color={color} />
-  </line>
+  </Group>
 );
 
 Line.propTypes = {
