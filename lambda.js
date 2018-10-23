e8ca4a9a0e294e296930511e015e10dd9806c87e
Sam Spilsbury <smspillaz@gmail.com>
2018-10-23 15:43:39 +0300

    lambda: Remove extra level of nesting for handler

--- lambda.js
+++ lambda.js
@@ -1,14 +1,6 @@
 const serverless = require('serverless-http');
 const app = require('./server/server');
 
-exports.handler = {
-  handler: serverless(app, {
-    binary: [
-      'font/eot',
-      'font/opentype',
-      'font/otf',
-      'image/jpeg',
-      'image/png',
-    ],
-  }),
-};
+exports.handler = serverless(app, {
+  binary: ['font/eot', 'font/opentype', 'font/otf', 'image/jpeg', 'image/png'],
+});
