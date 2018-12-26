1ed15401cf43c68fdcc266c34f6765b4866deb1e
Sam Spilsbury <smspillaz@gmail.com>
2018-12-26 17:42:54 +0800

    webpack: Use correct extension on require

--- internals/webpack/webpack.dev.server.babel.js
+++ internals/webpack/webpack.dev.server.babel.js
@@ -5,7 +5,7 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-module.exports = require('./webpack.dev.babel.js')({
+module.exports = require('./webpack.dev.babel')({
   entry: [path.join(process.cwd(), 'server/index.js')],
 
   externals: [nodeExternals()],
