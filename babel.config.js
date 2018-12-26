5f2788f36109f1f1bbc5170b8321ab5a172d735d
Sam Spilsbury <smspillaz@gmail.com>
2018-12-26 17:42:13 +0800

    babel: Use the babel-proposal-rest-spread plugin

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = {
   plugins: [
     'styled-components',
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
   ],
   env: {
