f76811e4ecba8563517746bb7e7a88731b08af37
Sam Spilsbury <smspillaz@gmail.com>
2018-12-26 17:42:40 +0800

    webpack: Make the dev server config extend the base one

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -4,28 +4,32 @@
 
 const webpack = require('webpack');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
+const config = require('./webpack.base.babel.js');
 
-module.exports = require('./webpack.base.babel')({
-  mode: 'development',
+module.exports = options =>
+  config({
+    mode: 'development',
 
-  optimization: {
-    minimize: false,
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {},
+    optimization: {
+      minimize: false,
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {},
+      },
     },
-  },
 
-  // Add development plugins
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new CircularDependencyPlugin({
-      exclude: /a\.js|node_modules/, // exclude node_modules
-      failOnError: false, // show a warning when there is a circular dependency
-    }),
-  ],
+    // Add development plugins
+    plugins: [
+      new webpack.NoEmitOnErrorsPlugin(),
+      new CircularDependencyPlugin({
+        exclude: /a\.js|node_modules/, // exclude node_modules
+        failOnError: false, // show a warning when there is a circular dependency
+      }),
+    ],
 
-  performance: {
-    hints: false,
-  },
-});
+    performance: {
+      hints: false,
+    },
+
+    ...options,
+  });
