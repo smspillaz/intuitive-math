1c144c89bb07a47d9f85072857d55f8c90050886
Sam Spilsbury <smspillaz@gmail.com>
2019-05-27 16:44:05 +0300

    app: Add HistoryContext

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,3 +1,10 @@
+import React from 'react';
 import { createBrowserHistory } from 'history';
 const history = createBrowserHistory();
 export default history;
+
+const HistoryContext = React.createContext({
+  location: null,
+});
+
+export { HistoryContext };
