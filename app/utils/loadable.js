0782ac090876e380c002869e331f79296600723a
Sam Spilsbury <smspillaz@gmail.com>
2019-05-27 17:38:33 +0300

    loadable: Suspense is not supported in SSR

--- app/utils/loadable.js
+++ app/utils/loadable.js
@@ -3,6 +3,12 @@ import React, { lazy, Suspense } from 'react';
 const loadable = (importFunc, { fallback = null } = { fallback: null }) => {
   const LazyComponent = lazy(importFunc);
 
+  // Suspense is not supported with server-side rendering yet. We will
+  // just need to wait until the client takes over
+  if (__SERVER__) {
+    return () => <div />;
+  }
+
   return props => (
     <Suspense fallback={fallback}>
       <LazyComponent {...props} />
