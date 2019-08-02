be9d1f1d731840127ba624d8c14d720d4c63450d
Sam Spilsbury <smspillaz@gmail.com>
2019-08-02 20:49:19 +0300

    loadable: Don't disable when testing

--- app/utils/loadable.js
+++ app/utils/loadable.js
@@ -2,14 +2,14 @@ import React, { lazy, Suspense } from 'react';
 
 const loadable = (importFunc, { fallback = null } = { fallback: null }) => {
   const LazyComponent = lazy(importFunc);
+  const isOnJSDOM =
+    navigator.userAgent.includes('Node.js') ||
+    navigator.userAgent.includes('jsdom');
+  const isTesting = process.env.NODE_ENV === 'test';
 
   // Suspense is not supported with server-side rendering yet. We will
   // just need to wait until the client takes over
-  if (
-    __SERVER__ ||
-    navigator.userAgent.includes('Node.js') ||
-    navigator.userAgent.includes('jsdom')
-  ) {
+  if (__SERVER__ || (isOnJSDOM && !isTesting)) {
     return () => <div />;
   }
 
