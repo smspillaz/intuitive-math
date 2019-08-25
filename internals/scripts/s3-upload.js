5b3f733e5a966df3f7a6e1ecc7d3b4129c531f74
Sam Spilsbury <smspillaz@gmail.com>
2019-08-25 15:33:00 +0300

    s3-upload: Delete old stuff in client/ before continuing
    Otherwise uploads might be skipped and we retain lots of garbage there.
    
    Better to just get rid of it.

--- internals/scripts/s3-upload.js
+++ internals/scripts/s3-upload.js
@@ -3,6 +3,7 @@ const path = require('path');
 const shell = require('shelljs');
 const s3EasyDeploy = require('s3-easy-deploy');
 const yargs = require('yargs');
+const AWS = require('aws-sdk');
 
 const argv =
   yargs
@@ -16,17 +17,40 @@ shell.cp('-R', 'build', path.join(s3UploadConfig.source, 'static'));
 process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || s3UploadConfig.accessKeyId;
 process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || s3UploadConfig.secretAccessKey;
 
-s3EasyDeploy.deploy({
-  publicRoot: s3UploadConfig.source,
-  bucket: s3UploadConfig.bucket,
-  region: s3UploadConfig.region,
-  acl: 'public-read',
-  metadata: [
-    {
-      match: /^.*$/,
-      tags: {
-        'Cache-Control': 'max-age=60'
+// Clean out build javascript objects in the static/ directory
+const s3Controller = new AWS.S3({ apiVersion: '2006-03-01' });
+s3Controller.listObjects({
+  Bucket: s3UploadConfig.bucket,
+  MaxKeys: 1000,
+}).promise().then(data =>
+  Promise.all(
+    data.Contents.map(d => d.Key)
+      .filter(k =>
+        k.startsWith('static') && (
+          k.endsWith('.js') ||
+          k.endsWith('.gz') ||
+          k.endsWith('.json)')
+        )
+      )
+      .map(k => s3Controller.deleteObject({
+        Bucket: s3UploadConfig.bucket,
+        Key: k
+      }).promise().then(() => console.log('Dropped object', k)))
+  )
+).then(() =>
+  s3EasyDeploy.deploy({
+    publicRoot: s3UploadConfig.source,
+    bucket: s3UploadConfig.bucket,
+    region: s3UploadConfig.region,
+    acl: 'public-read',
+    metadata: [
+      {
+        match: /^.*$/,
+        tags: {
+          'Cache-Control': 'max-age=60'
+        }
       }
-    }
-  ]
-}).then(() => console.log('Deployent to S3 bucket done')).catch(e => console.error(e));
+    ]
+  })
+).then(() => console.log('Deployent to S3 bucket done')).catch(e => console.error(e));
+
