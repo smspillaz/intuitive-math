89a2a7abab62750ea68ddc1a71ec15fadceb2811
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-11-19 18:40:37 +0200

    - Fix and refactor sample app:   - Merged App and HomePage slices   - RepoListItem becomes a component since we determine ownership of repo in the saga   - Delete all useless actions/constants/reducer files   - Update all broken tests - Add ESLint exception for reducer files

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -3,11 +3,35 @@
  */
 
 import { createSelector } from '@reduxjs/toolkit';
-import { initialState } from './reducer';
+import { initialState } from './slice';
 
 const selectHome = state => state.home || initialState;
 
 const makeSelectUsername = () =>
   createSelector(selectHome, homeState => homeState.username);
 
-export { selectHome, makeSelectUsername };
+const makeSelectLoading = () =>
+  createSelector(
+    selectHome,
+    homeState => homeState.loading,
+  );
+
+const makeSelectError = () =>
+  createSelector(
+    selectHome,
+    homeState => homeState.error,
+  );
+
+const makeSelectRepos = () =>
+  createSelector(
+    selectHome,
+    homeState => homeState.repositories,
+  );
+
+export {
+  selectHome,
+  makeSelectUsername,
+  makeSelectLoading,
+  makeSelectError,
+  makeSelectRepos,
+};
