c7c5337dec0b913cd80619bde5ec378ae50f1464
Sam Spilsbury <smspillaz@gmail.com>
2020-02-16 12:33:00 +0200

    webpack: For OfflinePlugin, static path is now /

--- internals/webpack/webpack.prod.client.babel.js
+++ internals/webpack/webpack.prod.client.babel.js
@@ -96,7 +96,7 @@ config.plugins.push.apply(config.plugins, [
   // assets manipulations and do leak its manipulations to HtmlWebpackPlugin
   new OfflinePlugin({
     relativePaths: false,
-    publicPath: '/static/',
+    publicPath: '/',
     appShell: '/',
 
     // No need to cache .htaccess. See http://mxs.is/googmp,
