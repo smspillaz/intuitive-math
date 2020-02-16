510fc388b16f855fce96ce69ad50e3da55392eb3
Sam Spilsbury <smspillaz@gmail.com>
2020-02-16 12:32:24 +0200

    addProdMiddlewares: Static path is now /

--- server/middlewares/addProdMiddlewares.js
+++ server/middlewares/addProdMiddlewares.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const ssr = require('../ssr');
 
 module.exports = function addProdMiddlewares(app, options) {
-  const publicPath = options.publicPath || '/static';
+  const publicPath = options.publicPath || '/';
   const outputPath = options.outputPath || path.resolve(process.cwd(), 'build');
 
   // use the gzipped bundle
