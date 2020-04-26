ba4708781dc567ab2f456fa6018e84ce9a51dfdb
Sam Spilsbury <smspillaz@gmail.com>
2020-04-26 22:48:29 +0300

    fixup: analytics formatting

--- app/analytics.js
+++ app/analytics.js
@@ -19,20 +19,18 @@ class SegmentAnalyticsApi {
     hasKey(window, 'analytics', () =>
       hasKey(window.analytics, 'load', () =>
         window.analytics.load(SEGMENT_CLIENT_ID),
-      )
+      ),
     );
   }
 
   pageView(category, name, props, options) {
     hasKey(window, 'analytics', () =>
-      window.analytics.page(category, name, props, options)
+      window.analytics.page(category, name, props, options),
     );
   }
 
   track(event, props) {
-    hasKey(window, 'analytics', () =>
-      window.analytics.track(event, props)
-    );
+    hasKey(window, 'analytics', () => window.analytics.track(event, props));
   }
 }
 
